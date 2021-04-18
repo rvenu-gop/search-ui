4c5362f004a9bbf62e9054ec9ab23372156f79bc
venuraja79 <venuraja79@gmail.com>
2021-04-18 20:41:01 +0530

    first version

---
+++ src/buildStateFacets.js
@@ -0,0 +1,62 @@
+function getValueFacet(aggregations, fieldName) {
+  if (
+    aggregations &&
+    aggregations[fieldName] &&
+    aggregations[fieldName].buckets &&
+    aggregations[fieldName].buckets.length > 0
+  ) {
+    return [
+      {
+        field: fieldName,
+        type: "value",
+        data: aggregations[fieldName].buckets.map(bucket => ({
+          // Boolean values and date values require using `key_as_string`
+          value: bucket.key_as_string || bucket.key,
+          count: bucket.doc_count
+        }))
+      }
+    ];
+  }
+}
+
+function getRangeFacet(aggregations, fieldName) {
+  if (
+    aggregations &&
+    aggregations[fieldName] &&
+    aggregations[fieldName].buckets &&
+    aggregations[fieldName].buckets.length > 0
+  ) {
+    return [
+      {
+        field: fieldName,
+        type: "range",
+        data: aggregations[fieldName].buckets.map(bucket => ({
+          // Boolean values and date values require using `key_as_string`
+          value: {
+            to: bucket.to,
+            from: bucket.from,
+            name: bucket.key
+          },
+          count: bucket.doc_count
+        }))
+      }
+    ];
+  }
+}
+
+export default function buildStateFacets(aggregations) {
+  const category = getValueFacet(aggregations, "category");
+  const title = getValueFacet(
+    aggregations,
+    "title"
+  );
+
+  const facets = {
+    ...(category && { category }),
+    ...(title && { title })
+  };
+
+  if (Object.keys(facets).length > 0) {
+    return facets;
+  }
+}
